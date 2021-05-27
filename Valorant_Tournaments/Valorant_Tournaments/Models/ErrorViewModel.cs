c03c11a2bf6954d68ff0cba85f227252aa889043
ThamerMashni <thamer.mashni@tuwaiq.edu.sa>
2021-05-27 21:20:37 +0300

    first commit

---
+++ Valorant_Tournaments/Valorant_Tournaments/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Valorant_Tournaments.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
