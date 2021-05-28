3ddb8ade75300f1d0e217b9dda90972c0c03739a
your-name <your-email@example.com>
2021-05-28 20:34:05 +0300

    make MVC project

---
+++ DiamondGallery/DiamondGallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DiamondGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
