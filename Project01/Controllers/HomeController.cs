f00a54401d188b53f7266aabd86586a5de1ec829
rawabe4 <rawabe44u@gmail.com>
2021-05-29 01:43:19 +0300

    Add Project01 Files

---
+++ Project01/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project01.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        string homeBar = "https://thermaltake.azureedge.net/pub/media/catalog/product/cache/e4fc6e308b66431a310dcd4dc0838059/l/2/l20m01.jpg";
+
+        public IActionResult Index()
+        {
+            ViewData["homeBar"] = homeBar;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Added
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project01/Controllers/Items.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project01.Models;
+
+namespace Project01.Controllers
+{
+    public class Items : Controller
+    {
+
+        List<ItemModel> itemsList = new List<ItemModel>() {
+           new ItemModel {ID=1, Name="Mango", Price=2393, Quantity= 1, Image ="https://www.luluhypermarket.com/medias/1428514-01.jpg-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wyODQ2OTd8aW1hZ2UvanBlZ3xpbWFnZXMvaDdiL2gxMC9oMDAvOTE5OTI2NjI5OTkzNC5qcGd8YWRhM2ZkODFiYjhmZDg3MzhmNzUwMzc1MmJlNTU0ZTQ4M2FhMTZiZDBjMDRlZThmYWZlMjUwZjIyNDQ2ZWNkYg", Description = "Fruits", Type="Fruit" },
+           new ItemModel {ID=1, Name="Banana", Price=2393, Quantity= 1, Image ="http://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466", Description = "Fruits", Type="Fruit" },
+           new ItemModel {ID=1, Name="Banana", Price=2393, Quantity= 1, Image ="http://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466", Description = "Fruits", Type="Fruit" },
+           new ItemModel {ID=1, Name="Banana", Price=2393, Quantity= 1, Image ="http://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466", Description = "Fruits", Type="Fruit" },
+           new ItemModel {ID=1, Name="Banana", Price=2393, Quantity= 1, Image ="http://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466", Description = "Fruits", Type="Fruit" },
+           new ItemModel {ID=1, Name="Banana", Price=2393, Quantity= 1, Image ="http://cdn.shopify.com/s/files/1/2154/4937/products/banana_600x.jpg?v=1610541466", Description = "Fruits", Type="Fruit" }
+
+        };
+        public IActionResult Shop()
+        {
+            return View(itemsList);
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
