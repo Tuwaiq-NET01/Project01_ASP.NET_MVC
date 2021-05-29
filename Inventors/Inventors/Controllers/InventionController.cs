608ca702bc1f269569d4e06e8a2edcc5813f7df5
your-name <your-email@example.com>
2021-05-29 17:52:48 +0300

    invention controller and model

--- Inventors/Inventors/Controllers/InventionController.cs
+++ Inventors/Inventors/Controllers/InventionController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inventors.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,21 @@ namespace Inventors.Controllers
 {
     public class InventionController : Controller
     {
+        List<Invention> InventorsList = new List<Invention>() {
+        new Invention {Id=1, Name="Vehicle with gas engine operation",
+                Image ="https://www.daimler.com/bilder/konzern/tradition/geschichte/anfaenge-des-automobils/benz-patent-motorwagen-w768xh384-cutout.png",
+                PatentDate = new DateTime( 1886, 1, 29), PatentNumber = "37435"},
+        new Invention {Id=2, Name="Electric arc lamp",
+                Image ="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Xenon_short_arc_1.jpg/1200px-Xenon_short_arc_1.jpg",
+                PatentDate = new DateTime(1886, 02, 09), PatentNumber = "335,786"},
+        new Invention {Id=3, Name="Commutator for dynamo-electric machines",
+                Image ="https://teslauniverse.com/sites/default/files/styles/default_extra_large_watermark/public/images/00334823-1.jpg?itok=PuAzN8Aq",
+                PatentDate = new DateTime(1886, 01, 26), PatentNumber = "334,823"},
+        new Invention {Id=4, Name="Alexander Graham Bell's telephone",
+                Image ="https://www.mozaweb.com/mozaik3D/TCH/gyartas/bell_telefonja/960.jpg",
+                PatentDate = new DateTime(1876, 03, 07), PatentNumber = "174,465"}
+    };
+
         public IActionResult Index()
         {
             return View();
--- Inventors/Inventors/Models/Invention.cs
+++ Inventors/Inventors/Models/Invention.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace Inventors.Models
 {
     public class Invention
     {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Image { set; get; }
+
+        [DataType(DataType.Date)]
+        public DateTime PatentDate { set; get; }
+        public string PatentNumber { set; get; }
     }
 }
