945c8f2c8b43d3488f0b7e8b5a44c98068366b5f
your-name <your-email@example.com>
2021-05-30 01:29:45 +0300

    fix card design

--- Inventors/Inventors/Controllers/InventionController.cs
+++ Inventors/Inventors/Controllers/InventionController.cs
@@ -15,13 +15,13 @@ namespace Inventors.Controllers
                 PatentDate = new DateTime( 1886, 1, 29), PatentNumber = "37435", Description=""},
         new Invention {Id=2, Name="Electric arc lamp",
                 Image ="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Xenon_short_arc_1.jpg/1200px-Xenon_short_arc_1.jpg",
-                PatentDate = new DateTime(1886, 02, 09), PatentNumber = "335,786"},
+                PatentDate = new DateTime(1886, 02, 09), PatentNumber = "335,786", Description=""},
         new Invention {Id=3, Name="Commutator for dynamo-electric machines",
                 Image ="https://teslauniverse.com/sites/default/files/styles/default_extra_large_watermark/public/images/00334823-1.jpg?itok=PuAzN8Aq",
-                PatentDate = new DateTime(1886, 01, 26), PatentNumber = "334,823"},
+                PatentDate = new DateTime(1886, 01, 26), PatentNumber = "334,823", Description=""},
         new Invention {Id=4, Name="Alexander Graham Bell's telephone",
                 Image ="https://www.mozaweb.com/mozaik3D/TCH/gyartas/bell_telefonja/960.jpg",
-                PatentDate = new DateTime(1876, 03, 07), PatentNumber = "174,465"}
+                PatentDate = new DateTime(1876, 03, 07), PatentNumber = "174,465", Description=""}
     };
 
         public IActionResult Index()
