4e02634a80d0f3bd0389a66c9986b860ae1a7032
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-05-29 20:23:04 +0300

    add API

--- Blog-Website/Blog-Website/Controllers/AuthorsController.cs
+++ Blog-Website/Blog-Website/Controllers/AuthorsController.cs
@@ -10,7 +10,7 @@ namespace Blog_Website.Controllers
     public class AuthorsController : Controller
     {
 
-        private List<UserModel> authors = new List<UserModel>() {
+        public List<UserModel> authors = new List<UserModel>() {
          new UserModel(1,"Kirsten Korosec","KirstenKorosec@gmail.com","https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco/tluillv6dybajfj7i0dq","https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb"),
          new UserModel(2,"Devin Coldewey","DevinColdewey@gmail.com","https://media-cldnry.s-nbcnews.com/image/upload/newscms/2014_01/95471/2503781351_7feef38a1a_b-95471.jpg","https://images.pexels.com/photos/3692749/pexels-photo-3692749.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
          new UserModel(3,"Rebecca Bellan","RebeccaBellan,@gmail.com","https://www.monteirolobato.edu.br/public/assets/admin/images/avatars/avatar5_big.png","https://images.pexels.com/photos/4500796/pexels-photo-4500796.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
