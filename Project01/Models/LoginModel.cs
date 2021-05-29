c2c80864b751ef8b39d5e659a80c13cd767894c2
rawabe4 <rawabe44u@gmail.com>
2021-05-29 17:58:47 +0300

    Update

---
+++ Project01/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project01.Models
+{
+    public class LoginModel
+    {
+        public string userName { get; set; }
+        public int password { get; set; }
+    }
+}
